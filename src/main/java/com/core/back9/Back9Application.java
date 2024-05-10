4cda51c16b3ce3582028e366f2a8c04e29404e0d
Geon.C <chlrjs1324@gmail.com>
2024-05-10 19:37:59 +0900

    initial commit

---
+++ src/main/java/com/core/back9/Back9Application.java
@@ -0,0 +1,13 @@
+package com.core.back9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Back9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Back9Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/core/back9/Back9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.back9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Back9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
