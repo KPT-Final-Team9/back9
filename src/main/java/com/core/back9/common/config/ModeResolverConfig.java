aa877e1779a34a220913ca1d991945ebeaf7d0b6
kyungho1224 <dev.kyungho.kim@gmail.com>
2024-05-20 00:01:06 +0900

    Add #21 : 오브젝트매퍼 설정을 스웨거에도 적용하도록 모델리졸버 설정 파일 추가

---
+++ src/main/java/com/core/back9/common/config/ModeResolverConfig.java
@@ -0,0 +1,16 @@
+package com.core.back9.common.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.core.jackson.ModelResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModeResolverConfig {
+
+	@Bean
+	public ModelResolver modelResolver(ObjectMapper objectMapper) {
+		return new ModelResolver(objectMapper);
+	}
+
+}
