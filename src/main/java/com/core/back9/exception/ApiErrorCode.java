26f307b2942c30cb5f68c7bcb6b0300f15494b5a
kyungho1224 <dev.kyungho.kim@gmail.com>
2024-05-22 09:54:39 +0900

    Fix : 웹에서 컨플릭트 수정해서 누락된 예외 추가

--- src/main/java/com/core/back9/exception/ApiErrorCode.java
+++ src/main/java/com/core/back9/exception/ApiErrorCode.java
@@ -10,6 +10,7 @@ public enum ApiErrorCode {
 
 	NOT_FOUND_VALID_BUILDING(HttpStatus.NOT_FOUND.value(), "유효한 건물을 찾을 수 없습니다"),
 	NOT_FOUND_VALID_ROOM(HttpStatus.NOT_FOUND.value(), "유효한 호실을 찾을 수 없습니다"),
+	NOT_FOUND_VALID_TENANT(HttpStatus.NOT_FOUND.value(), "유효한 입주사을 찾을 수 없습니다"),
 	DELETE_FAIL(HttpStatus.BAD_REQUEST.value(), "삭제가 완료되지 않았습니다."),
 	THREAD_POOL_REJECTED(HttpStatus.REQUEST_TIMEOUT.value(), "더 이상 요청을 처리 할 수 없습니다"),
 	DO_NOT_HAVE_PERMISSION(HttpStatus.UNAUTHORIZED.value(), "권한이 없습니다"),
