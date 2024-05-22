a1de00463f114e6c5a788cd8d97fa1c30ca9c8c1
kyungho1224 <dev.kyungho.kim@gmail.com>
2024-05-22 18:43:26 +0900

    Add #48 : 메일 발송 설정 추가

---
+++ src/main/java/com/core/back9/common/config/MailConfig.java
@@ -0,0 +1,56 @@
+package com.core.back9.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+	@Value("${spring.mail.protocol}")
+	private String protocol;
+
+	@Value("${spring.mail.properties.mail.smtp.auth}")
+	private String auth;
+
+	@Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+	private String starttls;
+
+	@Value("${spring.mail.host}")
+	private String host;
+
+	@Value("${spring.mail.port}")
+	private int port;
+
+	@Value("${spring.mail.username}")
+	private String username;
+
+	@Value("${spring.mail.password}")
+	private String password;
+
+	@Value("${spring.mail.default-encoding}")
+	private String encoding;
+
+	@Bean
+	public JavaMailSender javaMailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		Properties properties = new Properties();
+		properties.put("mail.transport.protocol", protocol);
+		properties.put("mail.smtp.auth", auth);
+		properties.put("mail.smtp.starttls.enable", starttls);
+
+		mailSender.setHost(host);
+		mailSender.setUsername(username);
+		mailSender.setPassword(password);
+		mailSender.setPort(port);
+		mailSender.setJavaMailProperties(properties);
+		mailSender.setDefaultEncoding(encoding);
+
+		return mailSender;
+	}
+
+}
