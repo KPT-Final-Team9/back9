29a945b4f6dd4510b12097c5349f74c55897169f
kyungho1224 <dev.kyungho.kim@gmail.com>
2024-05-22 18:43:56 +0900

    Add, Feat #48 : 메일 발송 컴포넌트 구현

---
+++ src/main/java/com/core/back9/common/service/MailManager.java
@@ -0,0 +1,30 @@
+package com.core.back9.common.service;
+
+import jakarta.mail.Message;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class MailManager {
+
+	@Value("${spring.mail.username}")
+	private String sender;
+
+	private final JavaMailSender javaMailSender;
+
+	public void send(String to, String title, String contents) throws Exception {
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+		mimeMessage.setFrom(sender);
+
+		mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+		mimeMessage.setSubject(title);
+		mimeMessage.setText(contents);
+		javaMailSender.send(mimeMessage);
+	}
+
+}
