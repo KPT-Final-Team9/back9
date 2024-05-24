9acc358558195b56513a657b213790191e839eaf
Geon.C <chlrjs1324@gmail.com>
2024-05-25 00:05:23 +0900

    Feat #53 : 에러 코드 추가

--- src/main/java/com/core/back9/exception/ApiErrorCode.java
+++ src/main/java/com/core/back9/exception/ApiErrorCode.java
@@ -12,6 +12,7 @@ public enum ApiErrorCode {
 	NOT_FOUND_VALID_ROOM(HttpStatus.NOT_FOUND.value(), "유효한 호실을 찾을 수 없습니다"),
     NOT_FOUND_VALID_TENANT(HttpStatus.NOT_FOUND.value(), "유효한 입주사를 찾을 수 없습니다"),
 	NOT_FOUND_VALID_CONTRACT(HttpStatus.NOT_FOUND.value(), "유효한 계약을 찾을 수 없습니다."),
+	ROOM_ALREADY_ASSIGNED(HttpStatus.BAD_REQUEST.value(), "이미 계약된 호실이 존재합니다."),
 	DELETE_FAIL(HttpStatus.BAD_REQUEST.value(), "삭제가 완료되지 않았습니다."),
 	THREAD_POOL_REJECTED(HttpStatus.REQUEST_TIMEOUT.value(), "더 이상 요청을 처리 할 수 없습니다"),
 	DO_NOT_HAVE_PERMISSION(HttpStatus.UNAUTHORIZED.value(), "권한이 없습니다"),
