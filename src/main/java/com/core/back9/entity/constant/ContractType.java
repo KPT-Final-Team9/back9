8264d745abbca4cc703d6df147957daf5d48ae69
Geon.C <chlrjs1324@gmail.com>
2024-05-24 23:59:31 +0900

    Feat #53 : contract 기본 기능 구현 - building, room, tenant와 연관관계 연결 완료 - 현재 재계약 검증부 로직에서 기간 관련 조정 로직은 미구현 상태 - Member 로직과 연결 및 미구현 기능 구현 완료 예정

---
+++ src/main/java/com/core/back9/entity/constant/ContractType.java
@@ -0,0 +1,6 @@
+package com.core.back9.entity.constant;
+
+public enum ContractType {
+    INITIAL, // 최초계약
+    RENEWAL // 재계약
+}
--- src/main/java/com/core/back9/service/ContractService.java
+++ src/main/java/com/core/back9/service/ContractService.java
@@ -2,11 +2,16 @@ package com.core.back9.service;
 
 import com.core.back9.dto.ContractDTO;
 import com.core.back9.entity.Contract;
+import com.core.back9.entity.Room;
+import com.core.back9.entity.Tenant;
+import com.core.back9.entity.constant.ContractType;
 import com.core.back9.entity.constant.Status;
 import com.core.back9.exception.ApiErrorCode;
 import com.core.back9.exception.ApiException;
 import com.core.back9.mapper.ContractMapper;
 import com.core.back9.repository.ContractRepository;
+import com.core.back9.repository.RoomRepository;
+import com.core.back9.repository.TenantRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,20 +26,63 @@ import java.util.stream.Collectors;
 @Service
 public class ContractService {
 
+    private final TenantRepository tenantRepository;
+    private final RoomRepository roomRepository;
     private final ContractRepository contractRepository;
     private final ContractMapper contractMapper;
 
     @Transactional
-    public ContractDTO.RegisterResponse registerContract(ContractDTO.RegisterRequest request) {
+    public ContractDTO.RegisterResponse registerContract(
+            Long buildingId,
+            Long roomId,
+            Long tenantId,
+            ContractDTO.RegisterRequest request
+    ) {
 
-        Contract contract = contractMapper.toEntity(request);
+        // 상위 데이터 유효성 검증
+        Tenant tenant = tenantRepository.getValidOneTenantOrThrow(tenantId);
+        Room room = roomRepository.getValidRoomWithIdOrThrow(buildingId, roomId, Status.REGISTER); // 해당 building의 room 조회
 
-        return contractMapper.toRegisterResponse(contractRepository.save(contract));
+        contractRepository.findByContractRoomIdAndTenantId(roomId, tenantId, ContractType.INITIAL)
+                .ifPresent(contract -> {
+                    throw new ApiException(ApiErrorCode.ROOM_ALREADY_ASSIGNED);
+                }); // 중복 데이터 검증(최초 계약이라는 정합성을 지켜야함!)
+
+        Contract validcontract = contractMapper.toEntity(request, tenant, room); // 연결 관계 매핑
+
+        return contractMapper.toRegisterResponse(contractRepository.save(validcontract));
     }
 
-    public ContractDTO.InfoList getAllContract(Pageable pageable) {
+    public ContractDTO.Info renewContract(
+            Long buildingId,
+            Long roomId,
+            Long contractId,
+            Long tenantId, ContractDTO.RegisterRequest request
+    ) {
 
-        Page<Contract> contracts = contractRepository.selectAllRegisteredContract(Status.REGISTER, pageable);
+        Tenant tenant = tenantRepository.getValidOneTenantOrThrow(tenantId);
+        Room room = roomRepository.getValidRoomWithIdOrThrow(buildingId, roomId, Status.REGISTER);
+
+        contractRepository.findByContractInitial(contractId, ContractType.INITIAL)
+                .orElseThrow(() -> new ApiException(ApiErrorCode.NOT_FOUND_VALID_CONTRACT)); // 초기 계약인지 검증
+
+        // TODO : 최초 계약의 기간에 따라 재계약 일자를 검증 & 설정하는 비즈니스 로직 필요, 현재는 재계약의 동작 여부를 확인하기 위한 정도만 구현함
+
+
+        Contract validContract = contractMapper.toEntity(request, tenant, room); // 엔티티 매핑
+
+        return contractMapper.toInfo(validContract);
+
+    }
+
+    public ContractDTO.InfoList getAllContract(
+            Long buildingId,
+            Long roomId,
+            Pageable pageable
+    ) {
+
+        Room room = roomRepository.getValidRoomWithIdOrThrow(buildingId, roomId, Status.REGISTER);
+        Page<Contract> contracts = contractRepository.selectAllRegisteredContract(room.getId(), Status.REGISTER, pageable);
 
         long count = contracts.getTotalElements();
 
@@ -46,13 +94,28 @@ public class ContractService {
 
     }
 
+    public ContractDTO.Info getOneContract(
+            Long buildingId,
+            Long roomId,
+            Long contractId
+    ) {
+
+        Room room = roomRepository.getValidRoomWithIdOrThrow(buildingId, roomId, Status.REGISTER);
+        Contract contract = contractRepository.getValidOneContractOrThrow(room.getId(), contractId);
+
+        return contractMapper.toInfo(contract);
+    }
+
     @Transactional
     public ContractDTO.Info modifyContract(
+            Long buildingId,
+            Long roomId,
             Long contractId,
             ContractDTO.UpdateRequest request
     ) {
 
-        Contract contract = contractRepository.getValidOneContractOrThrow(Status.REGISTER, contractId);
+        Room room = roomRepository.getValidRoomWithIdOrThrow(buildingId, roomId, Status.REGISTER);
+        Contract contract = contractRepository.getValidOneContractOrThrow(room.getId(), contractId);
         Contract updatedContract = contract.infoUpdate(request);
 
         return contractMapper.toInfo(updatedContract);
@@ -60,18 +123,17 @@ public class ContractService {
     }
 
     @Transactional
-    public Integer deleteContract(Long contractId) {
+    public Integer deleteContract(
+            Long buildingId,
+            Long roomId,
+            Long contractId
+    ) {
 
-        return contractRepository.deleteRegisteredContract(Status.UNREGISTER, contractId)
+        Room room = roomRepository.getValidRoomWithIdOrThrow(buildingId, roomId, Status.REGISTER);
+        return contractRepository.deleteRegisteredContract(Status.UNREGISTER, room.getId(), contractId)
                 .filter(result -> result != 0)
                 .orElseThrow(() -> new ApiException(ApiErrorCode.DELETE_FAIL));
 
     }
 
-    public ContractDTO.Info getOneContract(Long contractId) {
-
-        Contract contract = contractRepository.getValidOneContractOrThrow(Status.REGISTER, contractId);
-
-        return contractMapper.toInfo(contract);
-    }
 }
