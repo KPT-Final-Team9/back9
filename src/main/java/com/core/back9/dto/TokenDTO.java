c5c302025b64179bd8938b83ff5b88cd925d0e41
Dev-eun <deveun23@gmail.com>
2024-05-24 15:31:00 +0900

    Add #29 : TokenDTO 생성

---
+++ src/main/java/com/core/back9/dto/TokenDTO.java
@@ -0,0 +1,16 @@
+package com.core.back9.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class TokenDTO {
+
+    private String token;
+
+}
