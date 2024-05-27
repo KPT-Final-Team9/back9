bea0dc9a62b62d9d896d1a24d3e0f0451eb70a94
Dev-eun <deveun23@gmail.com>
2024-05-27 17:06:22 +0900

    Modify #63 : Member 조회 시 Tenant Fetch join하도록 수정

--- src/main/java/com/core/back9/repository/MemberRepository.java
+++ src/main/java/com/core/back9/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package com.core.back9.repository;
 import com.core.back9.entity.Member;
 import com.core.back9.entity.constant.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,6 +11,7 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    @Query("select m from member m join fetch m.tenant where m.email = :email and m.status = :status")
     Optional<Member> findByEmailAndStatus(String email, Status status);
 
     Optional<Member> findByPhoneNumberAndStatus(String phoneNumber, Status status);
