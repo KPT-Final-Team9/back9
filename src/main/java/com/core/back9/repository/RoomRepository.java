cc053ec233344742e9a64ff34a903c3c3cc988d9
Geon.C <chlrjs1324@gmail.com>
2024-05-28 15:58:06 +0900

    Feat #65 : Contract 과정에서 호실 검증을 위한 추가 쿼리문 작성

--- src/main/java/com/core/back9/repository/RoomRepository.java
+++ src/main/java/com/core/back9/repository/RoomRepository.java
@@ -7,6 +7,7 @@ import com.core.back9.exception.ApiException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -20,6 +21,16 @@ public interface RoomRepository extends JpaRepository<Room, Long> {
 		return findFirstByBuildingIdAndIdAndStatus(buildingId, roomId, status)
 		  .orElseThrow(() -> new ApiException(ApiErrorCode.NOT_FOUND_VALID_ROOM));
 	}
+
+	@Query("""
+			select r
+			from Room r
+			where r.building.id=?1
+			and r.id=?2
+			and r.member.id=?3
+			and r.status=?4
+			""")
+	Optional<Room> getRoomBySpecificIds(Long buildingId, Long roomId, Long memberId, Status status);
 
 	Page<Room> findAllByBuildingIdAndStatus(Long buildingId, Status status, Pageable pageable);
 
