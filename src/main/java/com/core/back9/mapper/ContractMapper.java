4b7647c718c5d1e25e8eafe13444890c525e259e
Geon.C <chlrjs1324@gmail.com>
2024-05-28 15:56:25 +0900

    Feat #65 : Contract 세부 보안 처리 구현과정에서 매핑에 필요한 추가 request, dto 작성

--- src/main/java/com/core/back9/mapper/ContractMapper.java
+++ src/main/java/com/core/back9/mapper/ContractMapper.java
@@ -6,9 +6,11 @@ import com.core.back9.entity.Room;
 import com.core.back9.entity.Tenant;
 import com.core.back9.entity.constant.ContractType;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Mapper(
@@ -20,11 +22,13 @@ public interface ContractMapper {
 
     Contract toEntity(ContractDTO.RegisterRequest registerRequest);
 
-    Contract toEntity(ContractDTO.RegisterRequest request, Tenant tenant, Room room, ContractType contractType);
-
     Contract toEntity(ContractDTO.UpdateRequest request);
 
+    Contract toEntity(ContractDTO.RegisterRequest request, Tenant tenant, Room room, ContractType contractType);
 
+    Contract toEntity(ContractDTO.RenewDto dto, Tenant tenant, Room room, ContractType contractType);
+
+    ContractDTO.RenewDto toDto(ContractDTO.RenewRequest request, LocalDate startDate);
 
     ContractDTO.RegisterResponse toRegisterResponse(Contract contract);
 
