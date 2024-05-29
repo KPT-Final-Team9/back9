e6b2a13d3a7123c34bc93b83e360d4e02331bb9d
Geon.C <chlrjs1324@gmail.com>
2024-05-30 02:43:32 +0900

    Feat #64: batch 관련 정보를 설정하는 batchConfig 생성 - 하나의 프로퍼티 객체를 job별로 재사용 및 추상화를 위해 Config 클래스를 생성해 프로퍼티를 관리

---
+++ src/main/java/com/core/back9/batch/property/BatchPropertyConfig.java
@@ -0,0 +1,19 @@
+package com.core.back9.batch.property;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BatchPropertyConfig {
+
+    @Bean("testBatchProperty")
+    public BatchProperty testBatchProperty() {
+        return new BatchProperty("testJob", true, "0 0 1 1 * *"); // 매년 1월 1일
+    }
+
+    @Bean("contractBatchProperty")
+    public BatchProperty contractBatchProperty() {
+        return new BatchProperty("contractJob", true, "0 0 1 1 * *"); // 매년 1월 1일
+    }
+
+}
