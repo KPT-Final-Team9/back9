f96bc834979463bb64a51b504d14d724e6d3853e
Geon.C <chlrjs1324@gmail.com>
2024-05-30 02:50:51 +0900

    Feat #64: BatchScheduler 생성
    - BatchScheduler 추상클래스에 스케줄러 내용 작성
    - BatchScheduler를 상속하는 자식 스케줄러를 구현해 도메인 별로 유연하게 스케줄러 생성 및 병렬 작동 가능하도록 구현

---
+++ src/main/java/com/core/back9/batch/scheduler/ContractBatchScheduler.java
@@ -0,0 +1,22 @@
+package com.core.back9.batch.scheduler;
+
+import com.core.back9.batch.job.BatchConfigFactory;
+import com.core.back9.batch.job.BatchConfig;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ContractBatchScheduler extends BatchScheduler {
+
+    public ContractBatchScheduler(JobLauncher jobLauncher, JobRegistry jobRegistry, BatchConfigFactory batchConfigFactory) {
+        super(jobLauncher, jobRegistry, batchConfigFactory);
+
+        BatchConfig batchConfig = batchConfigFactory.getBatchConfig("contractBatchConfig");
+
+        if(batchConfig.getBatchProperty().isJobEnabled()) {
+            startScheduler(batchConfig.getIdentifier());
+        }
+
+    }
+}
---
+++ src/main/java/com/core/back9/batch/scheduler/TestBatchScheduler.java
@@ -0,0 +1,21 @@
+package com.core.back9.batch.scheduler;
+
+import com.core.back9.batch.job.BatchConfigFactory;
+import com.core.back9.batch.job.BatchConfig;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestBatchScheduler extends BatchScheduler {
+
+    public TestBatchScheduler(JobLauncher jobLauncher, JobRegistry jobRegistry, BatchConfigFactory batchConfigFactory) {
+        super(jobLauncher, jobRegistry, batchConfigFactory);
+
+        BatchConfig batchConfig = batchConfigFactory.getBatchConfig("testBatchConfig");
+
+        if(batchConfig.getBatchProperty().isJobEnabled()) {
+            startScheduler(batchConfig.getIdentifier());
+        }
+    }
+}
