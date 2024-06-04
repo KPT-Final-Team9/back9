d307c5cdc64e1c7407dd3ff0b9a6b04ada1e88ab
Dev-eun <deveun23@gmail.com>
2024-06-04 15:36:14 +0900

    Feat : CORS 설정 추가

--- src/main/java/com/core/back9/common/config/WebMvcConfig.java
+++ src/main/java/com/core/back9/common/config/WebMvcConfig.java
@@ -6,6 +6,7 @@ import com.core.back9.security.AuthMemberResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,6 +17,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     private final MemberRepository memberRepository;
     private final MemberMapper memberMapper;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
