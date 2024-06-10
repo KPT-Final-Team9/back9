f6fa820809408e8390f519f26cdd1089c2f3c817
Geon.C <chlrjs1324@gmail.com>
2024-06-10 18:07:38 +0900

    Chore #146 : 미사용 코드 삭제

--- src/main/java/com/core/back9/batch/property/BatchPropertyConfig.java
+++ src/main/java/com/core/back9/batch/property/BatchPropertyConfig.java
@@ -6,11 +6,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class BatchPropertyConfig {
 
-    @Bean("testBatchProperty")
-    public BatchProperty testBatchProperty() {
-        return new BatchProperty("testJob", true, "0 0 0 1 1 *"); // 매년 1월 1일
-    }
-
     @Bean("contractBatchProperty")
     public BatchProperty contractBatchProperty() {
         return new BatchProperty("contractJob", true, "0 0 0 1 1 *"); // 매년 1월 1일
